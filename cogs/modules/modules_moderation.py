7a09e2c3d12f28d553b49ad12db5a36b34814372
Edsel Di Meo <edselmanuel@hotmail.com>
2020-08-16 13:30:09 -0500

    moderation modules cleaned

--- cogs/modules/modules_moderation.py
+++ cogs/modules/modules_moderation.py
@@ -14,9 +14,6 @@ def check_user_existance(ctx, user_id, bot):
     else: 
 
         return 0
-    
-
-    
 
 def check_roles(user_roles, roles_allowed):
 
@@ -32,23 +29,6 @@ def check_roles(user_roles, roles_allowed):
 
         return 0
 
-def check_item_attribute(category,item_key, value, settings):
-
-    """Checks the current value inside an item stored in the list of any attribute"""
-
-    for item in settings[category]:
-
-        for key in item:
-
-            if item_key == key:
-
-                if item[key] == value:
-
-                    return 1
-
-    else:
-
-        return 0
 
 def check_role_existance(role_check, roles_stored):
 
@@ -61,145 +41,6 @@ def check_role_existance(role_check, roles_stored):
     else:
 
         return 0
-
-
-
-async def alert_admins(settings, censured_channel, author, bot):
-
-        """Sends a message to every channel in admin_alert_channels when a user
-        has broken the rule 3 or more times"""
-
-        list_of_words_deleted = "Words deleted: "
-
-        for user in settings['censored_people']:
-
-            if user['author'] == author:
-
-                for word in user['words_deleted']:
-
-                    list_of_words_deleted += f' {word},'
-                    
-
-        for channel in settings['admin_alert_channels']:
-
-            if channel['language'] == 'ES':
-
-                await bot.get_channel(channel['channel_id']).send(f"<@{author}> acaba de escribir 3 o mas veces una palabra"
-                                            +f" prohibida en {censured_channel}\n\n {list_of_words_deleted}")
-
-                
-
-            else:
-
-                await bot.get_channel(channel['channel_id']).send(f"<@{author}> just wrote 3 times or more times a forbidden word"
-                                            +f" in {censured_channel}\n\n {list_of_words_deleted}")
-                                
-
-def word_avaliability_checker(banned_word, settings):
-
-        """Checks whether the banned word is already stored or not"""
-
-        valid_words = []
-
-        if len(banned_word) == 0:
-
-            if banned_word[0] in settings['banned_words']:
-
-                return False
-
-            else:
-
-                return valid_words.append(banned_word[0])
-
-        else:
-
-            for word in banned_word:
-
-                if not word in settings['banned_words']:
-
-                    valid_words.append(word)
-
-
-            if len(valid_words) > 0:
-
-                return valid_words
-
-            else:
-
-                return False
-
-def censoring_in_channel(channel_id,feature_checking, settings):
-
-    """Checks whether delete_messages feature is on or off and so do with the alert_users feature"""
-
-    for censored_channel in settings['channels_to_censor']:
-
-        if censored_channel['channel_id'] == channel_id:
-
-            for key in censored_channel:
-
-                if key == feature_checking:
-
-                    if feature_checking == 'delete_messages':
-
-                        if censored_channel[key].upper() == 'TRUE':
-
-                            return 1
-
-                        else:
-
-                            return 0
-
-                    elif feature_checking == 'alert_users':
-
-                        if censored_channel[key].upper() == 'PM':
-
-                            return 1
-
-                        elif censored_channel[key].upper() == 'CC':
-
-                            return 2
-
-                        elif censored_channel[key].upper() == 'RC':
-
-                            return 3
-
-                        elif censored_channel[key].upper() == 'BOTH':
-
-                            return 4
-
-                        else:
-
-                            return 0
-
-                    else:
-
-                        if censored_channel[key].upper() == 'TRUE':
-
-                            return 1
-
-                        else:
-
-                            return 0
-
-            
-def channel_avaliability_checker(channel_id, category, settings):
-
-        """Checks whether the channel is already stored or not"""
-
-        channels_stored = []
-
-        for channel in settings[category]:
-
-            channels_stored.append(channel['channel_id'])
-
-        if int(channel_id) in channels_stored:
-
-            return False
-
-        else:
-
-            return True
 
 def channel_existance_name(ctx, channel_name):
 
@@ -256,28 +97,6 @@ def category_existance(category_db, guild_categories):
             return 0 #doesn't exist
 
 
-
-
-async def send_message_privetly( forbidden_word, message_content, channel_name, author, channel_language, bot):
-
-        """This function sends a private message to someone"""
-
-        if channel_language.upper() == 'EN':
-
-            await bot.get_user(author).send('You just sent this'
-            +f' message in #{channel_name}:\n\n{message_content}\n\n and we wanted to let you know'
-            +f' that the word **{forbidden_word}** is prohibited in both #beginner_english'
-            +f' and #español_principiante')
-
-        else:
-
-            await bot.get_user(author).send('Acabas de mandar este'
-            +f' mensaje en #{channel_name}:\n\n{message_content}\n\n y te queriamos hacer saber'
-            +f' que la palabra **{forbidden_word}** está prohibida tanto en #beginner_english'
-            +f' y #español_principiante')
-
-
-
 def get_channel_name(channel_id, bot):
 
     """returns the name of a channel using its ID"""
@@ -289,74 +108,6 @@ def get_channel_name(channel_id, bot):
             return channel.name
 
 
-def banned_words_logger(author_id, author_nickname, banned_word, channel, jump_url , path):
-
-    """writes in a file all the users who has written a banned word"""
-
-    with open(f'{path}/banned_words_logger.txt','a') as f:
-
-        f.write(f"\n[{time.asctime()}] by:{author_id}({author_nickname}) channel: {channel} banned_word: {banned_word} url: {jump_url}")
-
-
-def print_info_attribute(category_name, category_info, bot, server_id):
-
-    """returns the info of an attribute"""
-
-    if category_name == 'channels_to_censor' or category_name == 'admin_alert_channels':
-
-        content_iterable = []
-
-        for key in category_info:
-
-            content_iterable.append(f"{key['channel_id']}({bot.get_channel(key['channel_id']).name} language:{key['language']}"
-            +f" delete messages:{key['delete_messages']} alert users:{key['alert_users']} admin alert:{key['admin_alert']}),\n ")
-
-        return message_maker(content_iterable, category_name)
-
-    elif category_name == 'censored_people':
-
-        content_iterable = []
-
-        for key in category_info:
-
-            content_iterable.append(f"{key['author']}({bot.get_guild(server_id).get_member(int(key['author'])).nick} infractions:{key['n_infractions']}), ")
-                
-        return message_maker(content_iterable, category_name)
-
-    
-    elif category_name == 'roles_allowed':
-
-        content_iterable = []
-
-        for role_id in category_info:
-
-            content_iterable.append(f"{role_id}({bot.get_guild(server_id).get_role(role_id).name}), ")
-                
-        return message_maker(content_iterable, category_name)
-
-    elif category_name == 'banned_words':
-
-        content_iterable = []
-
-        for word in category_info:
-
-            content_iterable.append(f'{word}, ')
-                
-        return message_maker(content_iterable, category_name)
-
-
-
-def message_maker(content, title):
-
-    """Creates a message using iterable content"""
-
-    message = f'```{title}:\n\n'
-    for string in content:
-
-                message += string
-    message += '```'
-    return message
-
 def category_verifier(category_sent, database):
     
     """Verifies if a category exists"""
@@ -366,79 +117,6 @@ def category_verifier(category_sent, database):
             return 1
     else:
         return 0
-
-
-
-async def censor_listener(message, banned_word, settings, bot):
-
-    """Manages user message depending on the word (if it's banned or not) and depending
-    channel censorship's settings"""
-
-    for channel in settings['channels_to_censor']:
-
-        if message.channel == bot.get_channel(channel['channel_id']):
-
-            for word in settings['banned_words']:
-
-                if f' {word.lower()} ' in banned_word.lower():
-
-                    if censoring_in_channel(message.channel.id, 'delete_messages', settings):
-
-                        await message.delete()
-
-                    if censoring_in_channel(message.channel.id, 'alert_users', settings) == 1:
-
-                        await send_message_privetly(word, message.content, message.channel.name, message.author.id, channel['language'], bot)
-
-                    if censoring_in_channel(message.channel.id, 'alert_users', settings) == 2:
-
-                        if check_item_attribute('channels_to_censor', 'language', 'ES', settings):
-
-                            await message.channel.send(f"**{word} está baneada! no la uses por favor!**")
-
-                        else:
-
-                            await message.channel.send(f"**{word} is banned! please don't use it**")
-
-                    if censoring_in_channel(message.channel.id, 'alert_users', settings) == 3:
-
-                        await message.add_reaction('❌')
-
-                    if censoring_in_channel(message.channel.id, 'alert_users', settings) == 4:
-
-                        await send_message_privetly(word, message.content, message.channel.name, message.author.id, channel['language'], bot)
-
-                        await message.channel.send(f"**{word} is banned! please don't use it**")
-
-                    if censoring_in_channel(message.channel.id,'admin_alert', settings) == 1:
-
-                        for person in settings['censored_people']:
-
-                                if person['author'] == message.author.id:
-
-                                    person['n_infractions'] += 1
-                                    person['words_deleted'].append(word)
-
-                                    if person['n_infractions'] % 3 == 0:
-
-                                        await alert_admins(settings, message.channel.name, person['author'], bot)
-
-                                    save(settings)
-
-                                    break
-
-                        else:
-
-                            settings['censored_people'].append({
-                                'author':message.author.id,
-                                'n_infractions': 1,
-                                'words_deleted':[word]
-                            })
-
-
-                        banned_words_logger(message.author, message.author.id ,word, message.channel.name, message.jump_url , dir_path)
-
-                        save(settings)
 
 
 async def message_correction_checker(bot, message, chat_language):
@@ -712,7 +390,7 @@ async def member_count_update(member, misc_settings):
 
                 try:
 
-                    await category.edit(name = f"{category_db['name']}({guild.member_count} MEMBERS)")
+                    await category.edit(name = f"{category_db['name']} ({guild.member_count} MEMBERS)")
 
                 except Forbidden:
 

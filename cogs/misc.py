92e07647eabb415dacb8ca72f4a3a7b39ae82cbe
Edsel Di Meo <edselmanuel@hotmail.com>
2021-01-19 15:11:12 -0500

    errors fixed

--- cogs/misc.py
+++ cogs/misc.py
@@ -109,7 +109,7 @@ class Misc(commands.Cog):
                 if stripped_msg[0] not in 'p=;!>' and len(stripped_msg) > 5:
 
                     #lang = await modules_misc.detect_language(stripped_msg)
-                    lang = await modules_misc.detect_language(stripped_msg)
+                    lang = modules_misc.detect_language(stripped_msg)
                     print(lang)
 
                     if(ultra_hardcore in message.author.roles or message.channel.id in self.misc_settings["nightmareMode"]["channels_id"] ): #Nightmare mode 
